c85bfdd70f3380b9f20dba163d191f64ad1efe2a
ShiMahiru <242531778@qq.com>
2025-09-11 17:41:56 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -42,11 +42,11 @@ background: {
 	},
 	favicon: [
 		// Leave this array empty to use the default favicon
-		// {
-		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
+		 {
+		   src: 'https://q2.qlogo.cn/headimg_dl?dst_uin=242531778&spec=0',    // Path of the favicon, relative to the /public directory
 		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
 		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-		// }
+		 }
 	],
 };
 
@@ -78,8 +78,13 @@ export const profileConfig: ProfileConfig = {
 	name: "ShiMahiru",
 	bio: "我知道你为了什么而来",
 	links: [
+      {
+      name: 'QQ',
+      icon: 'fa6-brands:qq',
+      url: 'https://qm.qq.com/q/jbMBofFJ4c',
+        },
 		{
-			name: "",
+			name: "Email",
 			icon: "fa6-solid:envelope",
 			url: "mailto:242531778@qq.com",
 		},
