26251dea04a0d32a713d2076d93103a993ec5583
ShiMahiru <242531778@qq.com>
2025-10-04 15:47:45 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "ShiMahiru Bolg",
-	subtitle: "在喧嚣世界里，收藏片刻宁静",
+	subtitle: "技术分享与实际",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 360, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
